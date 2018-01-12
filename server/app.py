14f95fb31138517bc850570677f6e9cf43447bb0
fcc <fcc.felippe@gmail.com>
2018-01-12 09:51:12 -0200

    adjust web service to new scrap workflow

--- server/app.py
+++ server/app.py
@@ -1,16 +1,16 @@
 from flask import Flask, request, jsonify
-from scrapper.recipes import TudoGostoso, TudoReceitas
+from scrapper.recipes import TudoGostoso, TudoReceitas, RecipeScrapper
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
+
 @app.route('/')
 def show_recipes():
     q = request.args.get('q')
-    tg = TudoGostoso(q)
-    tr = TudoReceitas(q)
-    # res = {**tg.results, **tr.results}
-    return jsonify([tg.results, tr.results])
+    parsers = [TudoGostoso, TudoReceitas]
+    scrapper = RecipeScrapper(parsers, q)
+    return jsonify(scrapper.scrape())
 
 
 if __name__ == '__main__':
